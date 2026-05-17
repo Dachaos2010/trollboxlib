9c231dfe0c2afe410b234beb4c0ad403e92abe4d
Dachaos <172603561+Dachaos2010@users.noreply.github.com>
2026-05-17 09:00:57 +0300

    do not throw any exceptions in socketuser

--- src/structs/socketuser.ts
+++ src/structs/socketuser.ts
@@ -37,30 +37,18 @@ export class SocketUser {
     /**
      * Checks if the user is you.
      * 
-     * @throws {NotConnectedError} If the client is not connected to the Trollbox.
-     * @throws {NotJoinedError} If the client have not joined the Trollbox.
-     * 
      * @returns {boolean}
      */
     is_me(): boolean {
-        if (!this.client.is_connected()) throw new NotConnectedError('Client is not connected');
-        if (!this.client.is_joined()) throw new NotJoinedError('Client have not joined');
-
         return this.client.socket?.id == this.client.users_manager.socket_id_by_user(this)
     }
 
     /**
      * Checks if the user is a king (first user in the users list).
      * 
-     * @throws {NotConnectedError} If the client is not connected to the Trollbox.
-     * @throws {NotJoinedError} If the client have not joined the Trollbox.
-     * 
      * @returns {boolean}
      */
     is_king(): boolean {
-        if (!this.client.is_connected()) throw new NotConnectedError('Client is not connected');
-        if (!this.client.is_joined()) throw new NotJoinedError('Client have not joined');
-
         return this.client.users_manager.king == this
     }
 }
