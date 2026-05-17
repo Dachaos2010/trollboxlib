3ab99d55309005b50b0706f1854cb1e996532bd7
Dachaos <172603561+Dachaos2010@users.noreply.github.com>
2026-05-17 09:00:57 +0300

    make client field in Context non nullable

--- src/context/context.ts
+++ src/context/context.ts
@@ -6,7 +6,7 @@ import type { TrollboxClient } from '../client.js';
  * @class
  */
 export class Context {
-    client: TrollboxClient | null = null;
+    client: TrollboxClient;
 
     /**
      * Creates a new instance.
